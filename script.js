502fa3e2be5d46996f5a516f1599edeac3e1a318
Ninja-Jutsu <ninja.jutsu.way@gmail.com>
2023-09-27 18:16:48 +0800

    Add CSS

--- script.js
+++ script.js
@@ -21,16 +21,16 @@ const seven = document.querySelector('#seven')
 const eight = document.querySelector('#eight')
 const zero = document.querySelector('#zero')
 
-one.onclick= () => { restartEqual();collectDigits(1);displayCollection(1);screenDisplay(); }
-two.onclick = () => { restartEqual();collectDigits(2);displayCollection(2);screenDisplay (); }
-three.onclick = () => { restartEqual();collectDigits(3);displayCollection(3);screenDisplay (); }
-four.onclick = () => { restartEqual();collectDigits(4);displayCollection(4);screenDisplay (); }
-five.onclick = () => { restartEqual();collectDigits(5);displayCollection(5);screenDisplay (); }
-six.onclick = () => { restartEqual();collectDigits(6);displayCollection(6);screenDisplay (); }
-seven.onclick = () => { restartEqual();collectDigits(7);displayCollection(7);screenDisplay (); }
-eight.onclick = () => { restartEqual();collectDigits(8); displayCollection(8);screenDisplay (); }
-nine.onclick = () => { restartEqual();collectDigits(9); displayCollection(9);screenDisplay (); }
-zero.onclick = () => { restartEqual();collectDigits(0); displayCollection('0');screenDisplay (); }
+one.onclick= () => { restartEqual();collectDigits(1);Collection(1);screenDisplay(); }
+two.onclick = () => { restartEqual();collectDigits(2);Collection(2);screenDisplay (); }
+three.onclick = () => { restartEqual();collectDigits(3);Collection(3);screenDisplay (); }
+four.onclick = () => { restartEqual();collectDigits(4);Collection(4);screenDisplay (); }
+five.onclick = () => { restartEqual();collectDigits(5);Collection(5);screenDisplay (); }
+six.onclick = () => { restartEqual();collectDigits(6);Collection(6);screenDisplay (); }
+seven.onclick = () => { restartEqual();collectDigits(7);Collection(7);screenDisplay (); }
+eight.onclick = () => { restartEqual();collectDigits(8); Collection(8);screenDisplay (); }
+nine.onclick = () => { restartEqual();collectDigits(9); Collection(9);screenDisplay (); }
+zero.onclick = () => { restartEqual();collectDigits(0); Collection('0');screenDisplay (); }
 
 // Operators:
 const equalBtn = document.querySelector('#equal-btn')
@@ -41,13 +41,13 @@ const subBtn = document.querySelector('#sub-btn')
 const addBtn = document.querySelector('#add-btn')
 const powBtn = document.querySelector('#Pow-btn')
 
-equalBtn.onclick = () => {splitArrays();displayCollection(currentResult);screenDisplay();equalityCounter =1;}
+equalBtn.onclick = () => {splitArrays();Collection(String(currentResult));screenDisplay();equalityCounter =1; console.log(screenShow); console.log(numbers)}
 refreshBtn.onclick= () => {refreshAll();screenDisplay ();}
-divideBtn.onclick = () => {refreshArray(); collectDigits('/');displayCollection('/');screenDisplay ();equalityCounter = 0}
-multiBtn.onclick = () => {refreshArray(); collectDigits('*');displayCollection('*');screenDisplay ();equalityCounter = 0}
-subBtn.onclick = () => {refreshArray(); collectDigits('-');displayCollection('-');screenDisplay ();equalityCounter = 0}
-addBtn.onclick = () => {refreshArray(); collectDigits('+');displayCollection('+');screenDisplay ();equalityCounter = 0}
-powBtn.onclick = () => {refreshArray(); collectDigits('^');displayCollection('^');screenDisplay ();equalityCounter = 0}
+divideBtn.onclick = () => {refreshArray(); collectDigits('/');Collection('/');screenDisplay ();equalityCounter = 0}
+multiBtn.onclick = () => {refreshArray(); collectDigits('*');Collection('*');screenDisplay ();equalityCounter = 0}
+subBtn.onclick = () => {refreshArray(); collectDigits('-');Collection('-');screenDisplay ();equalityCounter = 0}
+addBtn.onclick = () => {refreshArray(); collectDigits('+');Collection('+');screenDisplay ();equalityCounter = 0}
+powBtn.onclick = () => {refreshArray(); collectDigits('^');Collection('^');screenDisplay ();equalityCounter = 0}
 
 // Split First Operand, Second Operand & Operator:
 function splitArrays(){
@@ -57,8 +57,8 @@ function splitArrays(){
     });
     const operatorSplit = numbers.slice(0,operatorIndex);
     const operatorSplit2 = numbers.slice(operatorIndex+1,numbers.length);
+    firstNumber = '0';
     for(i=0;i<operatorSplit.length;i++){
-        firstNumber = '';
         firstNumber += operatorSplit[i];
     }
     for(i=0;i<operatorSplit2.length;i++){
@@ -70,23 +70,23 @@ function splitArrays(){
 // Operations:
 function operate(firstNumber, operator, secondNumber){
     if(operator == '+'){
-        let result =  add(parseInt(firstNumber),parseInt(secondNumber));
+        let result =  add(parseFloat(firstNumber),parseFloat(secondNumber));
         return result
     }
     else if(operator == '-'){
-        let result = subtract(parseInt(firstNumber),parseInt(secondNumber));
+        let result = subtract(parseFloat(firstNumber),parseFloat(secondNumber));
         return result
     }
     else if(operator == '*'){
-        let result = multiply(parseInt(firstNumber),parseInt(secondNumber))
+        let result = multiply(parseFloat(firstNumber),parseFloat(secondNumber))
         return result
     }
     else if(operator == '/'){
-        let result = divide(parseInt(firstNumber),parseInt(secondNumber))
+        let result = divide(parseFloat(firstNumber),parseFloat(secondNumber))
         return result
     }
     else if(operator == '^'){
-        let result = toPower(parseInt(firstNumber),parseInt(secondNumber));
+        let result = toPower(parseFloat(firstNumber),parseFloat(secondNumber));
         return result
     }
     else if(operator == 'c'){
@@ -106,9 +106,9 @@ const screen = document.querySelector('#screen')
 function screenDisplay (){
      screen.innerHTML = screenShow;
 }
-function displayCollection (n){
+function Collection (n){
     if (n == firstNumber){
-        screenShow = firstNumber;
+        screenShow = String(firstNumber);
     }
     else if (screenShow === '0'){
     screenShow = '';
@@ -127,7 +127,12 @@ function displayCollection (n){
 
 // delete button:
 const deleteBtn = document.querySelector('#delete-btn');
-deleteBtn.onclick = () => {if(screenShow.length <= 1){screenShow = '0'} else{screenShow =  screenShow.slice(0,-1); numbers.pop()}; screenDisplay ()}
+deleteBtn.onclick = () => 
+{
+    if(screenShow.length < 1){numbers.splice(0,numbers.length)} 
+    else{screenShow = screenShow.slice(0,-1); numbers.pop(); firstNumber = firstNumber.toString().slice(0,-1);}
+    screenDisplay ();
+}
 
 // Refresh on new operand entered after (=): 
 function restartEqual(){
@@ -137,9 +142,9 @@ function restartEqual(){
     }
 };
 
-
 function refreshArray(){
     if(equalityCounter === 1){
     numbers.splice(0,numbers.length);
-    numbers.push(firstNumber);}
+    numbers.push(firstNumber);
+    console.log(currentResult)}
 }
